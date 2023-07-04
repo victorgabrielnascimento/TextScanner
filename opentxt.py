0a1024c39b052be471851eb1f39877b1f5280528
{von} <105366808+victorgabrielnascimento@users.noreply.github.com>
2023-07-04 09:09:15 -0300

    New Update

--- opentxt.py
+++ opentxt.py
@@ -1,16 +1,20 @@
 import cv2
-from PIL import Image
 import pytesseract
-from pyzbar import pyzbar
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 camera = cv2.VideoCapture(0)
 
 while True:
-    ret, image = camera.read()
-    cv2.imshow('Scanner', image);
-
-    serialNumber = pytesseract.image_to_string(image)
+    ret, frame = camera.read()
+    cv2.imshow('Scanner', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    preprocessed_image = cv2.medianBlur(gray, 3)
+    text = pytesseract.image_to_string(preprocessed_image)
+    print("Texto detectado: ", text)
+
+camera.release()
+cv2.destroyAllWindows()
